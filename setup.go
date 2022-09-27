8021433566e8f9fc0a30f7d495aca22651ee9794
lixuan <work_lixuan@foxmail.com>
2022-09-27 14:22:49 +0800

    修改全局配置结构体

--- setup.go
+++ setup.go
@@ -1,14 +1,28 @@
 package mouselib
 
-import "github.com/rs/zerolog"
+import (
+	"strings"
 
+	"github.com/rs/zerolog"
+)
+
+// MouseConfig mouselib全局配置
+//
+// Mode 模式，debug/prod，测试和生产均使用prod
+//
+// LogPath 日志存储位置
 type MouseConfig struct {
-	Logger zerolog.Logger
+	Mode    string
+	LogPath string
 }
 
 var logger zerolog.Logger = DebugLogger()
 
 // SetUp 初始化库中的一些全局变量以及初始化客户端需要的参数，不显式调用则使用默认值
-func SetUp(config MouseConfig) {
-	logger = zerolog.Logger{}
+func SetUp(config MouseConfig) error {
+	if strings.ToLower(config.Mode) == "debug" {
+		logger = DebugLogger()
+	}
+
+	return nil
 }
