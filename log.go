8d30fd6061b93b86042d5722d9b2b4ae5431090a
lixuan <work_lixuan@foxmail.com>
2022-10-13 18:11:54 +0800

    日志模块不再打印日志，开箱即用

--- log.go
+++ log.go
@@ -11,8 +11,13 @@ import (
 	"github.com/rs/zerolog"
 )
 
-// DebugLogger 开发阶段直使用的logger
-func DebugLogger() zerolog.Logger {
+// CommandLogger 命令行logger，可以直接使用，默认level为DEBUG
+func CommandLogger(level string) (zerolog.Logger, error) {
+	l, err := zerolog.ParseLevel(level)
+	if err != nil {
+		return zerolog.Logger{}, err
+	}
+
 	wr := zerolog.NewConsoleWriter(func(w *zerolog.ConsoleWriter) {
 		w.NoColor = false
 		w.TimeFormat = "2006-01-02 15:04:05"
@@ -28,7 +33,25 @@ func DebugLogger() zerolog.Logger {
 		}
 	})
 
-	return zerolog.New(wr).With().Timestamp().Caller().Logger().Level(zerolog.DebugLevel)
+	return zerolog.New(wr).With().Timestamp().Caller().Logger().Level(l), nil
+}
+
+// FileLogger 文件logger，错误必须处理，如果路径无效则创建logger失败
+// 目前固定每天归档日志
+func FileLogger(filePath string, roll int, level string) (zerolog.Logger, error) {
+	l, err := zerolog.ParseLevel(level)
+	if err != nil {
+		return zerolog.Logger{}, err
+	}
+
+	wr, err := NewFileLoggerWriter(filePath, roll, l)
+	if err != nil {
+		return zerolog.Logger{}, err
+	}
+
+	multi := zerolog.MultiLevelWriter(wr, os.Stderr)
+
+	return zerolog.New(multi).With().Timestamp().Logger().Level(l), nil
 }
 
 type FileLoggerWriter struct {
@@ -38,43 +61,66 @@ type FileLoggerWriter struct {
 	Error io.Writer
 	Fatal io.Writer
 
-	t   time.Time
-	dir string
+	t    time.Time
+	roll int
+	dir  string
 }
 
-var levels = []string{"debug", "info", "warn", "error", "fatal"}
+var levels = []zerolog.Level{zerolog.DebugLevel, zerolog.InfoLevel, zerolog.WarnLevel, zerolog.ErrorLevel, zerolog.FatalLevel}
 
-func NewFileLoggerWriter(filePath string) (*FileLoggerWriter, error) {
-	e := make(chan error, 1)
-
-	wr := FileLoggerWriter{}
-
-	if _, err := os.Stat(filePath); err != nil {
-		err = os.MkdirAll(filePath, os.ModeDir|0700)
-		if err != nil {
-			e <- nil
-			return nil, err
-		}
+func formatLevel(l zerolog.Level) string {
+	switch l {
+	case zerolog.DebugLevel:
+		return "debug"
+	case zerolog.InfoLevel:
+		return "info"
+	case zerolog.WarnLevel:
+		return "warn"
+	case zerolog.ErrorLevel:
+		return "error"
+	case zerolog.FatalLevel:
+		return "fatal"
 	}
 
-	created := make([]*os.File, 0)
+	return ""
+}
+
+func NewFileLoggerWriter(filePath string, roll int, level zerolog.Level) (*FileLoggerWriter, error) {
+	e := make(chan error, 1)
 	defer func() {
 		if <-e != nil {
-			for _, v := range created {
-				os.Remove(v.Name())
+			err := os.RemoveAll(filePath)
+			if err != nil {
+				os.Exit(1)
 			}
 		}
 	}()
 
-	for _, level := range levels {
-		npath := filepath.Join(filePath, level+".log")
+	if _, err := os.Stat(filePath); err != nil {
+		err = os.MkdirAll(filePath, os.ModeDir|0700)
+		if err != nil {
+			e <- err
+			return nil, err
+		}
+	}
+
+	wr := FileLoggerWriter{}
+
+	levls := make([]zerolog.Level, 0)
+	for _, l := range levels {
+		if level <= l {
+			levls = append(levls, l)
+		}
+	}
+
+	for _, l := range levls {
+		npath := filepath.Join(filePath, formatLevel(l)+".log")
 		if _, err := os.Stat(npath); err != nil {
-			f, err := os.Create(npath)
+			_, err := os.Create(npath)
 			if err != nil {
 				e <- err
 				return nil, err
 			}
-			created = append(created, f)
 		}
 
 		f, err := os.OpenFile(npath, os.O_WRONLY|os.O_APPEND, 0700)
@@ -82,16 +128,26 @@ func NewFileLoggerWriter(filePath string) (*FileLoggerWriter, error) {
 			e <- err
 			return nil, err
 		}
-		created = append(created, f)
+
+		if l == zerolog.DebugLevel {
+			wr.Debug = f
+		}
+		if l == zerolog.InfoLevel {
+			wr.Info = f
+		}
+		if l == zerolog.WarnLevel {
+			wr.Warn = f
+		}
+		if l == zerolog.ErrorLevel {
+			wr.Error = f
+		}
+		if l == zerolog.FatalLevel {
+			wr.Fatal = f
+		}
 	}
 
-	wr.Debug = created[0]
-	wr.Info = created[1]
-	wr.Warn = created[2]
-	wr.Error = created[3]
-	wr.Fatal = created[4]
-
 	wr.t = time.Now()
+	wr.roll = roll
 	wr.dir = filePath
 
 	e <- nil
@@ -99,8 +155,7 @@ func NewFileLoggerWriter(filePath string) (*FileLoggerWriter, error) {
 }
 
 func (f *FileLoggerWriter) archive(ot time.Time) error {
-	logger.Debug().Msg("开始归档日志...")
-	suffix := fmt.Sprintf("%d%02d%02d", ot.Year(), ot.Month(), ot.Day())
+	suffix := fmt.Sprintf("%d%02d%02d%02d%02d%02d", ot.Year(), ot.Month(), ot.Day(), ot.Hour(), ot.Minute(), ot.Second())
 	created := make([]string, 0)
 	e := make(chan error, 1)
 	defer func() {
@@ -116,18 +171,17 @@ func (f *FileLoggerWriter) archive(ot time.Time) error {
 
 	olds := make([]string, 0)
 	for _, level := range levels {
-		lpath := filepath.Join(f.dir, level+".log")
+		lpath := filepath.Join(f.dir, formatLevel(level)+".log")
 		of, _ := os.Open(lpath)
 
-		opath := filepath.Join(odir, level+".log."+suffix)
+		opath := filepath.Join(odir, formatLevel(level)+".log."+suffix)
 		f, err := os.Create(opath)
 		if err != nil {
 			e <- err
 			return err
 		}
 
-		i, err := io.Copy(f, of)
-		logger.Debug().Int64("copy", i).Msg("copied")
+		_, err = io.Copy(f, of)
 		if err != nil {
 			e <- err
 			return err
@@ -143,7 +197,6 @@ func (f *FileLoggerWriter) archive(ot time.Time) error {
 
 	e <- nil
 	f.t = time.Now()
-	logger.Debug().Msg("归档日志完成")
 	return nil
 }
 
@@ -157,7 +210,7 @@ func (f *FileLoggerWriter) Write(p []byte) (n int, err error) {
 
 	ct, _ := time.Parse(time.RFC3339, ori.TimeStamp)
 	ot := f.t
-	if ct.Day()-ot.Day() >= 1 {
+	if ct.Second()-ot.Second() >= f.roll {
 		err := f.archive(ot)
 		if err != nil {
 			logger.Err(err).Msg("归档日志发生错误")
@@ -175,24 +228,7 @@ func (f *FileLoggerWriter) Write(p []byte) (n int, err error) {
 		f.Error.Write(p)
 	case "fatal":
 		f.Fatal.Write(p)
-	default:
-		print(ori.Level)
 	}
+
 	return len(p), nil
-}
-
-func ProdLogger(filePath string, hook zerolog.Hook) (zerolog.Logger, error) {
-	wr, err := NewFileLoggerWriter(filePath)
-	if err != nil {
-		return zerolog.Logger{}, err
-	}
-
-	multi := zerolog.MultiLevelWriter(wr, os.Stderr)
-
-	logger := zerolog.New(multi).With().Timestamp().Logger()
-
-	if hook != nil {
-		logger = logger.Hook(hook)
-	}
-	return logger, nil
 }
--- log_test.go
+++ log_test.go
@@ -7,14 +7,49 @@ import (
 	"github.com/mouseleee/mouselib"
 )
 
-func TestDebugLogger(t *testing.T) {
-	l := mouselib.DebugLogger()
+func TestDebugLoggerLevel(t *testing.T) {
+	ls := []string{"debug", "info", "warn", "error", "fatal", "mewo"}
 
-	l.Debug().Str("test", "bob").Msg("test print")
+	for i, v := range ls {
+		_, err := mouselib.CommandLogger(v)
+		if i != 5 && err != nil {
+			t.Error(err)
+		}
+		if i == 5 && err == nil {
+			t.Fail()
+		}
+	}
 }
 
-func TestProdLogger(t *testing.T) {
-	l, err := mouselib.ProdLogger("./log", nil)
+func TestDebugLoggerLog(t *testing.T) {
+	ls := []string{"debug", "info", "warn", "error", "fatal"}
+
+	for i, v := range ls {
+		l, err := mouselib.CommandLogger(v)
+		if err != nil {
+			t.Error(err)
+		}
+		if i == 0 {
+			l.Debug().Msg("test debug")
+		}
+		if i == 1 {
+			l.Debug().Msg("test info")
+		}
+		if i == 2 {
+			l.Info().Msg("test warn")
+		}
+		if i == 3 {
+			l.Warn().Msg("test error")
+		}
+		if i == 4 {
+			l.Error().Msg("test fatal")
+		}
+
+	}
+}
+
+func TestFileLogger(t *testing.T) {
+	l, err := mouselib.FileLogger("./log", 5, "debug")
 	if err != nil {
 		t.Error(err)
 	}
@@ -22,7 +57,7 @@ func TestProdLogger(t *testing.T) {
 	l.Debug().Str("test", "bob").Msg("test print")
 	l.Info().Str("test", "bob").Msg("test print")
 	l.Warn().Str("test", "bob").Msg("test print")
-	time.Sleep(4 * time.Second)
+	time.Sleep(15 * time.Second)
 	l.Error().Str("test", "bob").Msg("test print")
 	l.Fatal().Str("test", "bob").Msg("test print")
 }
