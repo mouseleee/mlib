10b87677f00623466ca8642a5799b4e3386ce39d
lixuan <work_lixuan@foxmail.com>
2022-11-28 12:02:50 +0800

    增加消费者组的删除、删除offset操作

--- mkafka/kafka.go
+++ mkafka/kafka.go
@@ -1,29 +1,32 @@
 package mkafka
 
 import (
+	"io"
+	"log"
+	"os"
 	"strings"
 	"time"
 
 	"github.com/Shopify/sarama"
+	"github.com/rs/zerolog"
 )
 
-// 提供一个默认的exactly-once的生产者和消费者，并且提供自定义配置来生成生产者盒消费者
+// 提供一个开箱即用的kafka生产者/消费者实例，保证exactly-once的消费逻辑
 //
-// 提供修改kafka集群配置，topic操作的方便api
+// 提供kafka客户端的部分功能，包括topic和consumer-group相关
 
-type KafkaErr struct {
-	msg string
-	err error
+var logger zerolog.Logger = zerolog.New(os.Stdout).With().Time("time", time.Now()).Caller().Logger()
+
+func SetLogger(out zerolog.Logger, saraLog bool) {
+	logger = out
+	if saraLog {
+		sarama.Logger = log.New(os.Stdout, "sarama->", log.Flags())
+	} else {
+		sarama.Logger = log.New(io.Discard, "[Sarama] ", log.LstdFlags)
+	}
 }
 
-func (e KafkaErr) Error() string {
-	return e.msg + " => " + e.err.Error()
-}
-
-func KafkaError(msg string, err error) KafkaErr {
-	return KafkaErr{msg: msg, err: err}
-}
-
+// DefaultProducerConfig 默认生产者配置
 func DefaultProducerConfig() *sarama.Config {
 	conf := sarama.NewConfig()
 	conf.Producer.Idempotent = true
@@ -35,15 +38,18 @@ func DefaultProducerConfig() *sarama.Config {
 	return conf
 }
 
+// DefaultProducer 默认生产者
 func DefaultProducer(brokers string) (sarama.SyncProducer, error) {
 	addrs := strings.Split(brokers, ",")
 	prd, err := sarama.NewSyncProducer(addrs, DefaultProducerConfig())
 	if err != nil {
-		return nil, KafkaError("创建生产者失败", err)
+		logger.Err(err).Msg("创建默认生产者失败")
+		return nil, err
 	}
 	return prd, nil
 }
 
+// DefaultConsumerConfig 默认消费者组配置
 func DefaultConsumerConfig() *sarama.Config {
 	conf := sarama.NewConfig()
 	conf.Consumer.Offsets.AutoCommit.Enable = false
@@ -51,37 +57,42 @@ func DefaultConsumerConfig() *sarama.Config {
 	return conf
 }
 
-func DefaultConsumer(brokers string, group string) (sarama.ConsumerGroup, error) {
+// DefaultConsumerGroup 默认消费者组
+func DefaultConsumerGroup(brokers string, group string) (sarama.ConsumerGroup, error) {
 	addrs := strings.Split(brokers, ",")
 	csm, err := sarama.NewConsumerGroup(addrs, group, DefaultProducerConfig())
 	if err != nil {
-		return nil, KafkaError("创建消费者失败", err)
+		logger.Err(err).Msg("创建默认消费者组失败")
+		return nil, err
 	}
 	return csm, nil
 }
 
-func CustomConsumer(brokers string, group string, config *sarama.Config) (sarama.ConsumerGroup, error) {
+// CustomConsumerGroup 根据传入的config参数创建消费者组
+func CustomConsumerGroup(brokers string, group string, config *sarama.Config) (sarama.ConsumerGroup, error) {
 	addrs := strings.Split(brokers, ",")
 	csm, err := sarama.NewConsumerGroup(addrs, group, config)
 	if err != nil {
-		return nil, KafkaError("创建消费者失败", err)
+		logger.Err(err).Msg("创建自定义消费者组失败")
+		return nil, err
 	}
 	return csm, nil
 }
 
-// CreateTopic 创建topic
-func CreateTopic(brokers string, topic string, partition int32, replica int16) error {
-	addrs := strings.Split(brokers, ",")
-	cli, err := sarama.NewClient(addrs, sarama.NewConfig())
-	if err != nil {
-		return KafkaError("初始化Kafka客户端失败", err)
-	}
-	defer cli.Close()
+// CreateKafkaClient 创建kafka客户端，使用后需关闭
+func CreateKafkaClient(brokers string, config *sarama.Config) (sarama.Client, error) {
+	addr := strings.Split(brokers, ",")
+	return sarama.NewClient(addr, config)
+}
 
-	ctrlr, err := cli.Controller()
+// CreateTopic 创建topic
+func CreateTopic(client sarama.Client, topic string, partition int32, replica int16) error {
+	ctrlr, err := client.RefreshController()
 	if err != nil {
-		return KafkaError("", err)
+		logger.Err(err).Msg("获取Controller失败")
+		return err
 	}
+	defer ctrlr.Close()
 
 	rsp, err := ctrlr.CreateTopics(&sarama.CreateTopicsRequest{
 		TopicDetails: map[string]*sarama.TopicDetail{
@@ -90,16 +101,83 @@ func CreateTopic(brokers string, topic string, partition int32, replica int16) e
 				ReplicationFactor: replica,
 			},
 		},
-		Timeout: 3 * time.Second,
+		Timeout: 5 * time.Second,
 	})
 	if err != nil {
-		return KafkaError("创建Topic请求发送失败", err)
+		logger.Err(err).Msg("发送创建Topic请求失败")
+		return err
 	}
-	err = nil
+
 	for t, e := range rsp.TopicErrors {
 		if e != nil {
-			err = KafkaError("topic "+t+"创建失败", e)
+			logger.Err(e).Str("topic", t).Msg("创建Topic失败")
+			return e
 		}
 	}
-	return err
+	return nil
+}
+
+// ResetConsumerGroupOffset 通过消费者组删除对某个topic的某些partition的offset
+func ResetConsumerGroupOffset(client sarama.Client, group string, allTopic bool, partitions map[string][]int32) error {
+	err := client.RefreshCoordinator(group)
+	if err != nil {
+		logger.Err(err).Msg("刷新coordinator失败")
+		return err
+	}
+	crdntr, err := client.Coordinator(group)
+	if err != nil {
+		logger.Err(err).Msg("获取coordinator失败")
+		return err
+	}
+	defer crdntr.Close()
+
+	req := new(sarama.DeleteOffsetsRequest)
+	req.Group = group
+	for k, v := range partitions {
+		for _, sv := range v {
+			req.AddPartition(k, sv)
+		}
+	}
+	res, err := crdntr.DeleteOffsets(req)
+	if err != nil {
+		logger.Err(err).Msg("发送删除指定topic的offset请求失败")
+		return err
+	}
+
+	for k, e := range res.Errors {
+		for p, se := range e {
+			if se != 0 {
+				logger.Err(se).Str("topic", k).Int32("partition", p).Msg("删除offset失败")
+				return se
+			}
+		}
+	}
+
+	return nil
+}
+
+func DeleteConsumerGroup(client sarama.Client, groups []string) error {
+	req := new(sarama.DeleteGroupsRequest)
+	req.Groups = groups
+
+	ctrlr, err := client.RefreshController()
+	if err != nil {
+		logger.Err(err).Msg("获取controller失败")
+		return err
+	}
+
+	rsp, err := ctrlr.DeleteGroups(req)
+	if err != nil {
+		logger.Err(err).Msg("发送删除消费者组请求失败")
+		return err
+	}
+
+	for k, e := range rsp.GroupErrorCodes {
+		if e != 0 {
+			logger.Err(e).Str("group", k).Msg("删除消费者组失败")
+			return e
+		}
+	}
+
+	return nil
 }
--- mkafka/test/kafka_test.go
+++ mkafka/test/kafka_test.go
@@ -1,87 +1,104 @@
 package mkafka_test
 
 import (
-	"context"
-	"fmt"
-	"log"
 	"os"
 	"testing"
 
-	"github.com/Shopify/sarama"
 	"github.com/mouseleee/mlib/mkafka"
+	"github.com/rs/zerolog"
 )
 
-var brokers = "localhost:9095"
+var brokers = "localhost:9093"
 
-func TestCreateTopic(t *testing.T) {
-	t.FailNow()
-	err := mkafka.CreateTopic(brokers, "mousetest", 1, 1)
+func TestSetLogger(t *testing.T) {
+	tl := zerolog.New(os.Stdout)
+	mkafka.SetLogger(tl, true)
+	mkafka.SetLogger(tl, false)
+}
+
+func TestDefaultProducerConfig(t *testing.T) {
+	mkafka.DefaultProducerConfig()
+}
+
+func TestDefaultProducer(t *testing.T) {
+	_, err := mkafka.DefaultProducer(brokers)
 	if err != nil {
 		t.Error(err)
 	}
 }
 
-func TestKafkaProduceMsg(t *testing.T) {
-	sarama.Logger = log.New(os.Stdout, "sarama=>", 0)
-	topic := "mousetest"
-	prd, err := mkafka.DefaultProducer(brokers)
+func TestDefaultConsumerConfig(t *testing.T) {
+	mkafka.DefaultConsumerConfig()
+}
+
+func TestDefaultConsumerGroup(t *testing.T) {
+	_, err := mkafka.DefaultConsumerGroup(brokers, "test.group")
 	if err != nil {
-		t.Error(err)
-	}
-	defer prd.Close()
-
-	count := 100
-
-	for count > 0 {
-		_, _, err := prd.SendMessage(&sarama.ProducerMessage{
-			Topic: topic,
-			Value: sarama.StringEncoder(fmt.Sprintf("这是第%d条测试消息", count)),
-		})
-		if err != nil {
-			t.Error(err)
-		}
-		count--
+		t.Error()
 	}
 }
 
-func TestKafkaConsumeMsg(t *testing.T) {
-	topic := "mousetest"
-	csm, err := mkafka.DefaultConsumer(brokers, "mousegroup")
-	if err != nil {
-		t.Error(err)
-	}
-	defer csm.Close()
+// func TestKafkaProduceMsg(t *testing.T) {
+// 	sarama.Logger = log.New(os.Stdout, "sarama=>", 0)
+// 	topic := "mousetest"
+// 	prd, err := mkafka.DefaultProducer(brokers)
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// 	defer prd.Close()
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	for {
-		err := csm.Consume(ctx, []string{topic}, &MyConsumer{})
-		if err != nil {
-			t.Error(err)
-		}
-	}
-}
+// 	count := 100
 
-type MyConsumer struct {
-}
+// 	for count > 0 {
+// 		_, _, err := prd.SendMessage(&sarama.ProducerMessage{
+// 			Topic: topic,
+// 			Value: sarama.StringEncoder(fmt.Sprintf("这是第%d条测试消息", count)),
+// 		})
+// 		if err != nil {
+// 			t.Error(err)
+// 		}
+// 		count--
+// 	}
+// }
 
-func (c *MyConsumer) Setup(session sarama.ConsumerGroupSession) error {
-	println("init...")
-	return nil
-}
+// func TestKafkaConsumeMsg(t *testing.T) {
+// 	topic := "mousetest"
+// 	csm, err := mkafka.DefaultConsumer(brokers, "mousegroup")
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// 	defer csm.Close()
 
-func (c *MyConsumer) Cleanup(session sarama.ConsumerGroupSession) error {
-	return nil
-}
+// 	ctx, cancel := context.WithCancel(context.Background())
+// 	defer cancel()
+// 	for {
+// 		err := csm.Consume(ctx, []string{topic}, &MyConsumer{})
+// 		if err != nil {
+// 			t.Error(err)
+// 		}
+// 	}
+// }
 
-func (c *MyConsumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
-	for {
-		select {
-		case msg := <-claim.Messages():
-			println(string(msg.Value))
-			session.Commit()
-		case <-session.Context().Done():
-			return nil
-		}
-	}
-}
+// type MyConsumer struct {
+// }
+
+// func (c *MyConsumer) Setup(session sarama.ConsumerGroupSession) error {
+// 	println("init...")
+// 	return nil
+// }
+
+// func (c *MyConsumer) Cleanup(session sarama.ConsumerGroupSession) error {
+// 	return nil
+// }
+
+// func (c *MyConsumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+// 	for {
+// 		select {
+// 		case msg := <-claim.Messages():
+// 			println(string(msg.Value))
+// 			session.Commit()
+// 		case <-session.Context().Done():
+// 			return nil
+// 		}
+// 	}
+// }
